4bef79c6a276e1dbc9de9972b9eecb5057dda2df
Sanjeev Premi <spremi@ymail.com>
2015-12-26 01:21:33 +0530

    client: Inject 'angular-material' in the application
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/app.js
+++ client/app/app.js
@@ -4,7 +4,8 @@ angular.module('preTtyApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ui.router'
+  'ui.router',
+  'ngMaterial'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
