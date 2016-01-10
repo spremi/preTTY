9fe6dc1e218a7a945aa8b62bdcc6cc6fd9083e23
Sanjeev Premi <spremi@ymail.com>
2016-01-10 10:49:55 +0530

    client: toast: Initial implementation
    'log' is injected into this controller
    when the mdToast object is created - where this
    controller is used.
    
    The function getClass() has been implemented
    to allow easy tweaks in the theme later on.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/controllers/toast/toast.controller.js
+++ client/app/controllers/toast/toast.controller.js
@@ -11,6 +11,51 @@
 'use strict';
 
 angular.module('preTtyApp')
-  .controller('ToastCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('ToastCtrl', [ '$scope', 'ptConst', '$mdToast', 'log',
+    function ($scope, ptConst, $mdToast, log) {
+
+      $scope.log = log;
+
+      /**
+       * Get the 'class' based on severity level of log.
+       */
+      $scope.getClass = function () {
+        var ret;
+
+        if (log.hasOwnProperty('level')) {
+          switch (log.level) {
+            case ptConst.LOG.DEBUG:
+              ret = 'md-accent';
+              break;
+
+            case ptConst.LOG.INFO:
+              ret = 'md-accent';
+              break;
+
+            case ptConst.LOG.WARN:
+              ret = 'md-warn';
+              break;
+
+            case ptConst.LOG.ERROR:
+              ret = 'md-warn';
+              break;
+
+            default:
+              ret = 'md-accent';
+              break;
+          }
+        } else {
+          ret = 'md-accent';
+        }
+
+        return ret;
+      };
+
+      /**
+       * Hide the toast
+       */
+      $scope.hide = function () {
+        $mdToast.hide();
+      };
+    }
+  ]);
