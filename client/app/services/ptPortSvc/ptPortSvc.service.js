a235022a9ada34961c3c26cefeebabce1f194d7a
Sanjeev Premi <spremi@ymail.com>
2016-01-10 11:18:57 +0530

    client: pt-port-svc: Add dummy data
    This data represents available serial ports.
    Information related to physical ports loosely
    extends the information provided by 'serialport'.
    
    This portpones need to work with real hardware
    for some time.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/services/ptPortSvc/ptPortSvc.service.js
+++ client/app/services/ptPortSvc/ptPortSvc.service.js
@@ -19,7 +19,32 @@ angular.module('preTtyApp')
        * All ports may not be currently active. But we may want to maintain
        * the data - in anticipation of reconnection.
        */
-      this.ports = [];
+      this.ports = [
+        {
+          id        : 'port0',
+          name      : 'Port 0',
+          connected : false,
+          phys      : {
+            comName       : '/dev/ttyUSB0',
+            manufacturer  : 'Dummy_Inc.',
+            serialNumber  : 'Dummy_Inc.0000',
+            vendorId      : '0x1234',
+            productId     : '0x5678'
+          }
+        },
+        {
+          id        : 'port1',
+          name      : 'Port 1',
+          connected : false,
+          phys      : {
+            comName       : '/dev/ttyUSB1',
+            manufacturer  : 'Dummy_Inc.',
+            serialNumber  : 'Dummy_Inc.0001',
+            vendorId      : '0x1234',
+            productId     : '0x567'
+          }
+        }
+      ];
 
       /**
        * Return list of available ports
