ade81e101941261681a49f3b2c012ff8c749e32b
Sanjeev Premi <spremi@ymail.com>
2016-01-10 10:43:58 +0530

    client: pt-log-svc: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/services/ptLogSvc/ptLogSvc.service.js
+++ client/app/services/ptLogSvc/ptLogSvc.service.js
@@ -11,6 +11,39 @@
 'use strict';
 
 angular.module('preTtyApp')
-  .service('ptLogSvc', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+  .service('ptLogSvc', [
+    function () {
+
+      /**
+       * Array of log messages
+       */
+      this.logs = [];
+
+
+      /**
+       * Appends message to the log
+       */
+      this.add = function (level, message) {
+        var log = {
+          'level'   : level,
+          'message' : message
+        };
+
+        this.logs.push(log);
+      };
+
+      /**
+       * Return list log messages
+       */
+      this.list = function () {
+        return this.logs;
+      };
+
+      /**
+       * Clears list of log messages
+       */
+      this.clear = function () {
+        this.logs.length = 0;
+      };
+    }
+  ]);
