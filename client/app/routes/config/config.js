023e3241ac912bb1c374f9a882fcc5a500252960
Sanjeev Premi <spremi@ymail.com>
2016-01-26 12:13:48 +0530

    client: /config: Add sub-routes 'list' and 'detail'
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/routes/config/config.js
+++ client/app/routes/config/config.js
@@ -14,18 +14,38 @@ angular.module('preTtyApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('config', {
+        abstract: true,
         url: '/config',
+        templateUrl: 'app/routes/config/config.html'
+      })
+      .state('config.list', {
+        url: '/list',
         views: {
-          '': {
-            templateUrl: 'app/routes/config/config.html'
-          },
           'head@config': {
             templateUrl: 'templates/header.html',
             controller: 'HeaderCtrl'
           },
           'body@config': {
-            templateUrl: 'app/routes/config/body.html',
-            controller: 'ConfigCtrl',
+            templateUrl: 'app/routes/config/body.list.html',
+            controller: 'ConfigCtrl'
+          },
+          'foot@config': {
+            templateUrl: 'templates/footer.html',
+            controller: 'FooterCtrl'
+          }
+        }
+      })
+      .state('config.detail', {
+        url: '/detail/:id',
+        controller: 'ConfigCtrl',
+        views: {
+          'head@config': {
+            templateUrl: 'templates/header.html',
+            controller: 'HeaderCtrl'
+          },
+          'body@config': {
+            templateUrl: 'app/routes/config/body.detail.html',
+            controller: 'ConfigCtrl'
           },
           'foot@config': {
             templateUrl: 'templates/footer.html',
