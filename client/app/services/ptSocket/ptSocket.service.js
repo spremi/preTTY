207ba29a606258f83a11ae81cf9ce030b9930194
Sanjeev Premi <spremi@ymail.com>
2016-02-13 23:40:08 +0530

    client: pt-socket: Initial implementation
    Implementation is based on the project created via
    angular-fullstack generator, but updated register()
    to return a function to be used for removing the
    event handler.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/services/ptSocket/ptSocket.service.js
+++ client/app/services/ptSocket/ptSocket.service.js
@@ -10,17 +10,56 @@
 
 'use strict';
 
+/* global io */
+
 angular.module('preTtyApp')
-  .factory('ptSocket', function () {
-    // Service logic
-    // ...
+  .factory('ptSocket', ['socketFactory',
+    function (socketFactory) {
 
-    var meaningOfLife = 42;
+      // socket.io now auto-configures its connection when we ommit a connection url
+      var ioSocket = io('', {
+        path: '/socket.io-client'
+      });
 
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
-    };
-  });
+      var socket = socketFactory({ioSocket: ioSocket});
+
+      /**
+        * Register handler for messages on the socket
+        *
+        * @param {String} Message
+        * @param {Function} Associated handler
+        *
+        * @returns Function to unregister the handler
+        */
+      var register = function (msg, cb) {
+        cb = cb || angular.noop;
+
+        var wrapper = function (data) {
+          cb(data);
+        };
+
+        socket.on(msg, wrapper);
+
+        return function () {
+          socket.removeListener(msg, wrapper);
+        };
+      };
+
+      /**
+        * Send message & data via socket
+        *
+        * @param {String} Message
+        * @param {Object} Associated data
+        */
+      var send = function (msg, data) {
+        socket.emit(msg, data);
+      };
+
+
+      return {
+        socket    : socket,
+        register  : register,
+        send      : send
+      };
+    }
+  ]);
